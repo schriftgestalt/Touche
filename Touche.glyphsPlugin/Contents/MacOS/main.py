10b7f43d0cd2c4b61326effa934d0b3474324781
schriftgestalt <georg.seifert@mac.com>
2019-04-29 02:37:02 +0200

    try to fix probelm with none ASCII chars in file name

--- Touche.glyphsPlugin/Contents/MacOS/main.py
+++ Touche.glyphsPlugin/Contents/MacOS/main.py
@@ -1,10 +1,23 @@
+# -*- coding: utf-8 -*-
 def _run(script):
 	global __file__
 	import os, sys
+	reload(sys)
+	sys.setdefaultencoding('utf8')
 	sys.frozen = 'macosx_plugin'
+	if not __file__ or __file__.find("Contents/Resources/") < 0:
 	base = os.environ['RESOURCEPATH']
-	path = os.path.join(base, script)
-	__file__ = path
-	execfile(path, globals(), globals())
+		__file__ = path = os.path.join(base, script)
+	else:
+		__file__ = path = __file__.replace("../MacOS/main.py", script)
+	
+	if sys.version_info[0] == 2:
+		with open(path, 'rU') as fp:
+			source = fp.read() + "\n"
+	else:
+		with open(path, 'r', encoding='utf-8') as fp:
+			source = fp.read() + '\n'
+
+	exec(compile(source, path, 'exec'), globals(), globals())
 
 _run('plugin.py')
